d41de95296fdc377b4c3a2f980c47f7d9ccc33d2
UDAZ <u.ka2.sally.cinnamon@gmail.com>
2021-07-11 10:32:01 +0900

    add processinfo

---
+++ processinfo/index.js
@@ -0,0 +1,11 @@
+// console.log(JSON.stringify(process.env, null, 2));
+
+//for (var i = 0; i < process.argv.length; i++) {
+//  console.log(`${i} : ${process.argv[i]}`);
+//}
+
+//console.log(`cwd(): ${process.cwd()}`);
+//console.log(`__dirname: ${__dirname}`);
+//実行位置とフォルダの位置
+
+console.log(process.platform);
