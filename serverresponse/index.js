b702f41cd544f4dbb04e8c0b9a005b998675f114
UDAZ <u.ka2.sally.cinnamon@gmail.com>
2021-07-21 23:19:10 +0900

    add client-post

--- serverresponse/index.js
+++ serverresponse/index.js
@@ -4,15 +4,15 @@ var path = require("path");
 
 var server = http.createServer((req, res) => {
   console.log(
-    `[${(new Date()).toISOString()}]` +
-    `${req.method} ${req.url}` +
+    `[${(new Date()).toISOString()}] ` +
+    `${req.method} ${req.url} - ` +
     `${req.headers["user-agent"]}`
   );
-  if(req.methods==="POST"){
+  if (req.method === "POST") {
     req.pipe(res);
-  }else{
+  } else {
     var reader = fs.createReadStream(path.join(__dirname, "sample.txt"), "utf8");
-    reader.pipe(res);
+    reader.pipe(res);  
   }
 });
 server.listen(3000);
