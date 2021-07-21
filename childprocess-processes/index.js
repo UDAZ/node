17a2a01fb22761e7fdfad5f0e3e4e7d5bf124930
UDAZ <u.ka2.sally.cinnamon@gmail.com>
2021-07-21 23:47:29 +0900

    add childprocess-processes

---
+++ childprocess-processes/index.js
@@ -0,0 +1,20 @@
+/*
+var {fork} = require("child_process");
+var path = require("path");
+
+var child = fork(path.join(__dirname, "child.js"), {execArgv:[]});
+child.on("message", (data)=>{
+  console.log(data);
+});
+child.on("close", (code)=>{
+  console.log(`child process exited with code ${code}`)
+});
+*/
+var {fork} = require("child_process");
+var path = require("path");
+
+var child = fork(path.join(__dirname, "child.js"), {execArgv:[]});
+child.send({hello: "message from parent."});
+child.on("close", (code)=>{
+  console.log(`child process exited with code ${code}`);
+});
